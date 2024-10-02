dbd80a3b7b2f7a281fa0c33e3390e6e35f23abf0
Karl <karlcereno@gmail.com>
2024-10-02 19:16:01 -0400

    Refactor NavBar component

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,23 +1,27 @@
 import { Link } from '@remix-run/react';
 
+const navLinks = [
+  { label: 'Products', href: '/', hiddenOnMobile: false },
+  { label: 'About', href: '/', hiddenOnMobile: true },
+  { label: 'FAQ', href: '/', hiddenOnMobile: true },
+  { label: 'Contact', href: '/', hiddenOnMobile: false },
+  { label: 'Cart', href: '/', hiddenOnMobile: false },
+];
+
 function NavBar() {
   return (
-    <nav className="flex gap-8 justify-center uppercase underline md:no-underline">
-      <Link to={''}>Products</Link>
-      <Link
-        className="hidden md:block"
-        to={''}
-      >
-        About
-      </Link>
-      <Link
-        className="hidden md:block"
-        to={''}
-      >
-        FAQ
-      </Link>
-      <Link to={''}>Contact</Link>
-      <Link to={''}>Cart</Link>
+    <nav className="flex gap-8 justify-center uppercase underline md:no-underline ">
+      {navLinks.map((navLink) => (
+        <Link
+          key={navLink.label}
+          to={navLink.href}
+          className={`hover:text-red-500 transition-colors duration-300 ${
+            navLink.hiddenOnMobile ? 'hidden md:block' : ''
+          }`}
+        >
+          {navLink.label}
+        </Link>
+      ))}
     </nav>
   );
 }
